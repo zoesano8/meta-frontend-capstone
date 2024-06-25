65e11f0ff88c5e8cec5dc9c422995c9d27e1d9be
zoesano8 <145507365+zoesano8@users.noreply.github.com>
2024-06-25 08:57:27 -0600

    Update App.js
    Remove unnecessary commented code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
-//import Header from './components/Header';
-//import BookingForm from './components/BookingForm';
 import Main from './components/Main';
 import Menu from './components/Menu';
 import Footer from './components/Footer';
@@ -20,7 +18,3 @@ function App() {
 }
 
 export default App;
-
-//<Header/>
-//<Main/>
-////<BookingForm/>
